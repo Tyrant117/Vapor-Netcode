930d54997556635cb76fffd0e6f643bf11e98f82
tyrant117 <nslange@ucdavis.edu>
2023-05-06 14:32:55 -0700

    c

--- Runtime/Data/ObservableFactory.cs
+++ Runtime/Data/ObservableFactory.cs
@@ -1,32 +1,22 @@
 using System;
 using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 
 namespace VaporNetcode
 {
-    public static partial class ObservableFactory
+    public static class ObservableFactory
     {
-        public static Dictionary<int, Func<int, ObservableClass>> factoryMap = new();
+        private static readonly Dictionary<int, Func<int, ObservableClass>> _factoryMap = new();
 
-        public static Dictionary<int, int> counterMap = new(200);
-        //Network IDs for Objects
-        private static int counter = 0;
-        public static int NextUniqueID(int type)
+        private static readonly Dictionary<int, int> _counterMap = new(100);
+        private static int NextUniqueID(int type)
         {
-            if (counterMap.TryGetValue(type, out var counter))
-            {
-                counterMap[type] = counter + 1;
-            }
-            else
-            {
-                counterMap[type] = 1;
-            }
+            _counterMap[type] = _counterMap.TryGetValue(type, out var counter) ? counter + 1 : 1;
 
-            int id = counterMap[type];
+            int id = _counterMap[type];
             if (id == int.MaxValue)
             {
-                throw new Exception("connection ID Limit Reached: " + id);
+                throw new Exception("ID Limit Reached: " + id);
             }
 
             if (NetLogFilter.logDebug && NetLogFilter.spew) { Debug.LogFormat("Generated Observable ID: {0}", id); }
@@ -35,19 +25,16 @@ namespace VaporNetcode
 
         static ObservableFactory()
         {
-            Initialize();
+
         }
 
-        static partial void Initialize();
-
-
 #pragma warning disable IDE0051 // Remove unused private members
-        private static void AddFactory(int id, Func<int, ObservableClass> factory) => factoryMap[id] = factory;
+        public static void AddFactory(int id, Func<int, ObservableClass> factory) => _factoryMap[id] = factory;
 #pragma warning restore IDE0051 // Remove unused private members
 
         public static bool TryCreateObservable<T>(int typeId, out T result) where T : ObservableClass
         {
-            if (factoryMap.TryGetValue(typeId, out var factory))
+            if (_factoryMap.TryGetValue(typeId, out var factory))
             {
                 result = factory.Invoke(NextUniqueID(typeId)) as T;
                 return true;
@@ -61,7 +48,7 @@ namespace VaporNetcode
 
         public static bool TryCreateObservable<T>(int typeId, int customID, out T result) where T : ObservableClass
         {
-            if (factoryMap.TryGetValue(typeId, out var factory))
+            if (_factoryMap.TryGetValue(typeId, out var factory))
             {
                 result = factory.Invoke(customID) as T;
                 return true;
